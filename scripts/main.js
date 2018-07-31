ca03ef8c117854079ce9ef440fa4d9e4b6060a4c
Jozef <jozefw@sbcglobal.net>
2018-07-31 14:24:33 -0700

    final commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    var jsonURL = books.json;
+    var jsonURL = "scripts/books.json";
+    $.getJSON(jsonURL,function(json){
+        var imgList = '';
+        $.each(json.book_list,function(){
+            imgList += '<li class="image__wrapper"><span class="image__title">'+ this.title +'</span><img class="image_pic" src= "' + this.imageLink + '"></li>';
+
+        });
+        $('.center-section__book-list').append(imgList);
+    });
 });
